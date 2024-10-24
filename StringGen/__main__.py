d2a9520c1e76f98f9228809e54c3328d8807f7be
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-24 19:43:42 +0530

    Update __main__.py

--- StringGen/__main__.py
+++ StringGen/__main__.py
@@ -1,11 +1,17 @@
 import asyncio
 import importlib
-
+import os
+import threading
+from flask import Flask
 from pyrogram import idle
-
 from StringGen import LOGGER, Anony
 from StringGen.modules import ALL_MODULES
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
 
 async def anony_boot():
     try:
@@ -20,7 +26,10 @@ async def anony_boot():
     LOGGER.info(f"@{Anony.username} Started.")
     await idle()
 
-
 if __name__ == "__main__":
+    # Start the Flask app in a separate thread
+    threading.Thread(target=lambda: app.run(host='0.0.0.0', port=int(os.getenv("PORT", 8000)))).start()
+    
+    # Start the Telegram bot
     asyncio.get_event_loop().run_until_complete(anony_boot())
     LOGGER.info("Stopping String Gen Bot...")
